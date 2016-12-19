6f6b008085ca7670152b69111336bbc73f346257
Aram Joseph <ajoseph@utc4me.org>
2016-12-19 10:32:30 -0500

    Party Has Been Started

---
+++ Assets/Scripts/Damager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    [SerializeField]
+    int health = 3;
+
+    void CheckDead()
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<Rigidbody2D>().Sleep();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Sword")
+        {
+            health--;
+            CheckDead();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    KeyCode attack;
+
+    private Animator anim;
+    private float moveHorizontal;
+    private float moveVertical;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        moveVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(moveHorizontal, moveVertical, 0f);
+        Animate();
+    }
+
+    void Animate()
+    {
+        if (moveHorizontal != 0 || moveVertical != 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("input_x", moveHorizontal);
+            anim.SetFloat("input_y", moveVertical);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            //anim.SetFloat("input_x", 0f);
+            //anim.SetFloat("input_y", 0f);
+        }
+        if (Input.GetKeyDown(attack))
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+}
