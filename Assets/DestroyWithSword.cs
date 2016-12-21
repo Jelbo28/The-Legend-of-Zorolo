26df849275939129169a3bb121cfddccca8de9e8
Aram Joseph <ajoseph@utc4me.org>
2016-12-21 10:31:34 -0500

    uhh

---
+++ Assets/DestroyWithSword.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWithSword : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] state;
+    private SpriteRenderer playerRender;
+	// Use this for initialization
+	void Start ()
+    {
+        playerRender = GetComponent<SpriteRenderer>();
+        playerRender.sprite = state[0];
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Sword" || other.tag == "Projectile")
+        {
+            playerRender.sprite = state[1];
+            other.gameObject.GetComponent<Damager>().Attacked(-1f);
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
